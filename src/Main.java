242ff9f7b7a0118995757440dc50b908d146b8cc
AlexGabor <alex.dan.gabor@gmail.com>
2015-10-18 16:10:39 +0300

    main function implemented.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,26 @@
-/**
- * Created by alex on 10/11/15.
- */
+import controller.Controller;
+import repository.IRepository;
+import repository.Repository;
+import ui.ASCIIUserInterface;
+
+import java.io.FileNotFoundException;
+
 public class Main {
 
     public static void main(String args[]) {
+        IRepository repository = new Repository();
+        Controller controller = new Controller(repository);
+        ASCIIUserInterface ui;
 
+        // alternative: read input from file.
+        /*try {
+            ui = new ASCIIUserInterface(controller, "input");
+            ui.run();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }*/
+
+        ui = new ASCIIUserInterface(controller);
+        ui.run();
     }
 }
